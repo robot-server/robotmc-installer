6ca48bd47d3894bac0337b4c3a1e22109ee1f231
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-06-10 23:04:28 +0900

    HTTP 클라이언트 로그 추가
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- src/main/kotlin/com/sysbot32/robotmc/installer/config/HttpClientConfig.kt
+++ src/main/kotlin/com/sysbot32/robotmc/installer/config/HttpClientConfig.kt
@@ -1,13 +1,23 @@
 package com.sysbot32.robotmc.installer.config
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestClient
+
+private val log = KotlinLogging.logger { }
 
 @Configuration
 class HttpClientConfig {
     @Bean
     fun restClient(): RestClient {
-        return RestClient.create()
+        return RestClient.builder()
+            .requestInterceptor { request, body, execution ->
+                log.info { "${request.method} ${request.uri}" }
+                val response = execution.execute(request, body)
+                log.info { "${response.statusCode}" }
+                return@requestInterceptor response
+            }
+            .build()
     }
 }
