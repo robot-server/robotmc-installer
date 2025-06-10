99a6ccf074c88948e7a3c8725f469730177fe8b9
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-06-10 22:23:40 +0900

    모드 로더 유형 추가
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

---
+++ src/main/kotlin/com/sysbot32/robotmc/installer/mod/loader/ModLoaderType.kt
@@ -0,0 +1,10 @@
+package com.sysbot32.robotmc.installer.mod.loader
+
+enum class ModLoaderType(
+    val displayName: String,
+) {
+    FORGE("Forge"),
+    NEO_FORGE("NeoForge"),
+    FABRIC("Fabric"),
+    QUILT("Quilt"),
+}
