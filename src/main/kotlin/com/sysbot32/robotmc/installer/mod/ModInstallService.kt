e1171c666dd7afe79c049f26943d0d2fe6bcc363
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-06-13 01:44:37 +0900

    모드 설치 서비스 추가
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

---
+++ src/main/kotlin/com/sysbot32/robotmc/installer/mod/ModInstallService.kt
@@ -0,0 +1,29 @@
+package com.sysbot32.robotmc.installer.mod
+
+import com.sysbot32.robotmc.installer.config.InstallerProperties
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestClient
+import java.nio.file.Files
+
+private val log = KotlinLogging.logger { }
+
+@Service
+class ModInstallService(
+    private val restClient: RestClient,
+    private val installerProperties: InstallerProperties,
+) {
+    fun install() {
+        val modsDir = installerProperties.minecraft.directory.resolve("mods").also { log.info { it } }
+        for (mod in installerProperties.mod?.mods ?: listOf()) {
+            val path = modsDir.resolve(mod.downloadUrl.split("/").last())
+            if (!Files.exists(path)) {
+                this.restClient.get()
+                    .uri(mod.downloadUrl)
+                    .retrieve()
+                    .toEntity(ByteArray::class.java)
+                    .body?.let { Files.write(path, it) }
+            }
+        }
+    }
+}
