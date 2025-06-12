9b9a52d13394d6085257440a6367f2ed8ad1ce89
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-06-13 01:47:29 +0900

    애플리케이션 실행 시 설치 과정 자동 실행
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- src/main/kotlin/com/sysbot32/robotmc/installer/RobotmcInstallerApplication.kt
+++ src/main/kotlin/com/sysbot32/robotmc/installer/RobotmcInstallerApplication.kt
@@ -1,11 +1,18 @@
 package com.sysbot32.robotmc.installer
 
+import com.sysbot32.robotmc.installer.mod.ModInstallService
+import com.sysbot32.robotmc.installer.mod.loader.ModLoaderInstallService
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import kotlin.system.exitProcess
 
 @SpringBootApplication
 class RobotmcInstallerApplication
 
 fun main(args: Array<String>) {
-    runApplication<RobotmcInstallerApplication>(*args)
+    runApplication<RobotmcInstallerApplication>(*args).run {
+        getBean(ModLoaderInstallService::class.java).install()
+        getBean(ModInstallService::class.java).install()
+        exitProcess(0)
+    }
 }
