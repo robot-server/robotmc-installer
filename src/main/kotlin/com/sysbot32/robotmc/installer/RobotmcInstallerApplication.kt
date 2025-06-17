3abbba8ca82fabb2c6900f643badd308a2da2654
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-06-17 21:41:55 +0900

    시작/종료 로그 추가
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- src/main/kotlin/com/sysbot32/robotmc/installer/RobotmcInstallerApplication.kt
+++ src/main/kotlin/com/sysbot32/robotmc/installer/RobotmcInstallerApplication.kt
@@ -2,17 +2,22 @@ package com.sysbot32.robotmc.installer
 
 import com.sysbot32.robotmc.installer.mod.ModInstallService
 import com.sysbot32.robotmc.installer.mod.loader.ModLoaderInstallService
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import kotlin.system.exitProcess
+
+private val log = KotlinLogging.logger { }
 
 @SpringBootApplication
 class RobotmcInstallerApplication
 
 fun main(args: Array<String>) {
     runApplication<RobotmcInstallerApplication>(*args).run {
+        log.info { "========== Start ==========" }
         getBean(ModLoaderInstallService::class.java).install()
         getBean(ModInstallService::class.java).install()
+        log.info { "========== End ==========" }
         exitProcess(0)
     }
 }
