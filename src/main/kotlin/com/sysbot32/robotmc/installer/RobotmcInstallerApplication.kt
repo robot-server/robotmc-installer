4dac7de78e1141631ed4861e9d58d09f0f341f1a
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-06-18 22:40:55 +0900

    헤드리스 모드 해제
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- src/main/kotlin/com/sysbot32/robotmc/installer/RobotmcInstallerApplication.kt
+++ src/main/kotlin/com/sysbot32/robotmc/installer/RobotmcInstallerApplication.kt
@@ -5,6 +5,7 @@ import com.sysbot32.robotmc.installer.mod.loader.ModLoaderInstallService
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.awt.GraphicsEnvironment
 import kotlin.system.exitProcess
 
 private val log = KotlinLogging.logger { }
@@ -13,6 +14,9 @@ private val log = KotlinLogging.logger { }
 class RobotmcInstallerApplication
 
 fun main(args: Array<String>) {
+    if (GraphicsEnvironment.isHeadless() && "nogui" !in args) {
+        System.setProperty("java.awt.headless", false.toString())
+    }
     runApplication<RobotmcInstallerApplication>(*args).run {
         log.info { "========== Start ==========" }
         getBean(ModLoaderInstallService::class.java).install()
