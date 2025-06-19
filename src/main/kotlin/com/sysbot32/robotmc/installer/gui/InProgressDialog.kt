6ac3a83715a50ae2fc3ae6f0c674a70eef7955e9
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-06-19 19:16:36 +0900

    GUI 창 화면 중앙으로 배치
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- src/main/kotlin/com/sysbot32/robotmc/installer/gui/InProgressDialog.kt
+++ src/main/kotlin/com/sysbot32/robotmc/installer/gui/InProgressDialog.kt
@@ -3,6 +3,8 @@ package com.sysbot32.robotmc.installer.gui
 import com.sysbot32.robotmc.installer.config.InstallerProperties
 import org.springframework.stereotype.Component
 import java.awt.Dimension
+import java.awt.Point
+import java.awt.Toolkit
 import javax.swing.JDialog
 import javax.swing.JProgressBar
 
@@ -15,6 +17,9 @@ class InProgressDialog(
     init {
         this.title = "설치 중"
         this.size = Dimension(400, 100)
+
+        val screenSize = Toolkit.getDefaultToolkit().screenSize
+        this.location = Point(screenSize.width / 2 - this.width / 2, screenSize.height / 2 - this.height / 2)
 
         this.add(this.progressBar)
     }
