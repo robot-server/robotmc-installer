b73e9c52dfd760b86d5f84ea15d899502346f474
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-06-23 22:51:22 +0900

    메인 스레드 이름 변경
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- src/main/kotlin/com/sysbot32/robotmc/installer/config/InstallerConfig.kt
+++ src/main/kotlin/com/sysbot32/robotmc/installer/config/InstallerConfig.kt
@@ -3,6 +3,7 @@ package com.sysbot32.robotmc.installer.config
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
+import java.util.*
 
 private val log = KotlinLogging.logger { }
 
@@ -12,6 +13,8 @@ class InstallerConfig(
     private val installerProperties: InstallerProperties,
 ) {
     init {
+        Thread.currentThread().name = UUID.randomUUID().toString()
+
         log.info { "Environments: ${System.getenv()}" }
         log.info { "Properties: ${System.getProperties()}" }
         log.info { "Java Runtime Version: ${Runtime.version()}" }
