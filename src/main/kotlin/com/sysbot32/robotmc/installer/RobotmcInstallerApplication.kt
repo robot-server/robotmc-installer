b7c6b1176e0f7bd5e90920814d2732c77f1ad61a
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-06-23 23:58:22 +0900

    예외 처리
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- src/main/kotlin/com/sysbot32/robotmc/installer/RobotmcInstallerApplication.kt
+++ src/main/kotlin/com/sysbot32/robotmc/installer/RobotmcInstallerApplication.kt
@@ -20,23 +20,36 @@ fun main(args: Array<String>) {
         System.setProperty("java.awt.headless", false.toString())
     }
     runApplication<RobotmcInstallerApplication>(*args).run {
-        if (JOptionPane.showConfirmDialog(
-                null,
-                "서버 접속에 필요한 모드 로더 및 모드를 설치할까요?\n기존 설치 모드는 mods_old로 옮겨집니다.",
-                "설치",
-                JOptionPane.YES_NO_OPTION
-            ) != JOptionPane.YES_OPTION
-        ) {
+        try {
+            if (JOptionPane.showConfirmDialog(
+                    null,
+                    "서버 접속에 필요한 모드 로더 및 모드를 설치할까요?\n기존 설치 모드는 mods_old로 옮겨집니다.",
+                    "설치",
+                    JOptionPane.YES_NO_OPTION
+                ) != JOptionPane.YES_OPTION
+            ) {
+                exitProcess(0)
+            }
+            log.info { "========== Start ==========" }
+            val inProgressDialog = getBean(InProgressDialog::class.java)
+            inProgressDialog.isVisible = true
+            getBean(ModLoaderInstallService::class.java).install()
+            getBean(ModInstallService::class.java).install()
+            inProgressDialog.isVisible = false
+            log.info { "========== End ==========" }
+            JOptionPane.showMessageDialog(null, "완료됐어요.")
             exitProcess(0)
+        } catch (e: Exception) {
+            log.error(e) { e.message }
+            if (!GraphicsEnvironment.isHeadless()) {
+                JOptionPane.showMessageDialog(
+                    null,
+                    "오류가 발생했어요.\n로그 파일 첨부하여 제보 부탁드려요.",
+                    e.message,
+                    JOptionPane.ERROR_MESSAGE
+                )
+            }
+            exitProcess(-1)
         }
-        log.info { "========== Start ==========" }
-        val inProgressDialog = getBean(InProgressDialog::class.java)
-        inProgressDialog.isVisible = true
-        getBean(ModLoaderInstallService::class.java).install()
-        getBean(ModInstallService::class.java).install()
-        inProgressDialog.isVisible = false
-        log.info { "========== End ==========" }
-        JOptionPane.showMessageDialog(null, "완료됐어요.")
-        exitProcess(0)
     }
 }
