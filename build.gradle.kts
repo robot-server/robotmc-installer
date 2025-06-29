e584786dfbb5b95d005d22de68650e0c76f80d62
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-06-29 17:33:01 +0900

    launch4j 플러그인 추가
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.5.0"
     id("io.spring.dependency-management") version "1.1.7"
+    id("edu.sc.seis.launch4j") version "3.0.6"
 }
 
 group = "com.sysbot32"
