f6518ac79cf4a845ad8865022c9539c70eefe58f
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-06-29 15:35:52 +0900

    progressbar 의존성 추가
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.7")
     implementation("org.apache.commons:commons-exec:1.4.0")
+    implementation("me.tongfei:progressbar:0.10.1")
     implementation("dev.dewy:nbt:1.5.1")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
